8217f8f6e81809fcfbc5c295bfd0a2c926513c3c
Aubrey Chipman <aubrey.chipman@gmail.com>
2015-01-02 17:06:53 -0600

    Jshint options stored in .jshintrc

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,25 +6,8 @@ module.exports = function (grunt) {
       all: {
         src: 'app/assets/javascripts/*.js',
         options: {
-          bitwise: true,
-          camelcase: true,
-          curly: true,
-          eqeqeq: true,
-          forin: true,
-          immed: true,
-          indent: 4,
-          latedef: true,
-          newcap: true,
-          noarg: true,
-          noempty: true,
-          nonew: true,
-          quotmark: 'single',
-          regexp: true,
-          undef: true,
-          unused: true,
-          trailing: true,
-          maxlen: 120
-        }
+          jshintrc: '.jshintrc'
+        },
       }
     },
 
