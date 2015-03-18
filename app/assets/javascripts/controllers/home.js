e31d8e5e3e8fe9d800c50e6ac4c37913d07a7e8c
Aubrey Chipman <aubrey.chipman@gmail.com>
2015-03-18 00:46:57 -0500

    First pass at updating angular templates with new variable on scope.

--- app/assets/javascripts/controllers/home.js
+++ app/assets/javascripts/controllers/home.js
@@ -5,5 +5,18 @@
         .module('Reclist')
         .controller('HomeCtrl', function ($scope) {
             $scope.things = ['Angular', 'Rails 4.1', 'UI Router', 'Together!!'];
+            $scope.recs = [
+                {   title: 'the yellow wallpaper',
+                    author: 'charlotte perkins gilman',
+                    rating: 8,
+                    summary: 'a chronicle of the narrator\'s descent into madness.'
+                },
+                {
+                    title: 'brave new world',
+                    author: 'aldous huxley',
+                    rating: 9,
+                    summary: 'a cautionary dystopian tale.'
+                }
+            ];
         });
 })();
